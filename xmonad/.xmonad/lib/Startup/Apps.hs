2a80f9167e4d72b722443f30ccfa6766158c4d26
DJP <djpade@gmail.com>
2021-08-26 21:29:40 -0400

    Reformat with ormolu

--- xmonad/.xmonad/lib/Startup/Apps.hs
+++ xmonad/.xmonad/lib/Startup/Apps.hs
@@ -1,7 +1,7 @@
 module Startup.Apps where
 
 import           Startup.Utils
-import           System.Environment (getEnv)
+import           System.Environment             ( getEnv )
 import           XMonad
 
 spawnedApps :: [String]
@@ -12,27 +12,27 @@ killSpawns = mapM_ (\x -> spawn ("killall " ++ x)) spawnedApps
 
 ioIconHeight :: IO Int
 ioIconHeight = do
-    ydpi <- yDpi
-    return $ floor $ (0.1667 :: Double) * fromIntegral ydpi
+  ydpi <- yDpi
+  return $ floor $ (0.1667 :: Double) * fromIntegral ydpi
 
 barWidth :: IO Int
 barWidth = do
-    (res, _) <- getResolution
-    return $ res - 10
+  (res, _) <- getResolution
+  return $ res - 10
 
 barOffset :: IO Int
 barOffset = do
-    (res, _) <- getResolution
-    return $ res `div` 2
+  (res, _) <- getResolution
+  return $ res `div` 2
 
 comptonCmd :: IO String
 comptonCmd = do
-    (xres, _) <- getResolution
-    home <- getEnv "HOME"
-    intHeight <- ioHeight
-    let height = show intHeight
-        compReg = xres - 10
-    return $ "picom -CGf"
+  (xres, _) <- getResolution
+  home      <- getEnv "HOME"
+  intHeight <- ioHeight
+  let height  = show intHeight
+      compReg = xres - 10
+  return $ "picom -CGf"
 
 flashfocusCmd :: IO String
 flashfocusCmd = return "flashfocus"
