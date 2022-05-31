7968c2cafae2951975fe0a754b0bd9075339f3f7
DJP <djpade@gmail.com>
2022-05-31 10:26:07 -0400

    Move XMonad colors to separate module

---
+++ xmonad/.xmonad/lib/Bar/Colors.hs
@@ -0,0 +1,87 @@
+module Bar.Colors where
+
+import           Numeric                        ( readHex
+                                                , showHex
+                                                )
+
+data Triple a = Triple (a, a, a)
+
+instance Functor Triple where
+  fmap f (Triple (x, y, z)) = Triple (f x, f y, f z)
+
+instance Applicative Triple where
+  pure x = Triple (x, x, x)
+  (Triple (f1, f2, f3)) <*> (Triple (x1, x2, x3)) =
+    (Triple (f1 x1, f2 x2, f3 x3))
+
+type Color = Triple Int
+
+string2col :: String -> Color
+string2col s = Triple (r, g, b)
+ where
+  s'       = drop 1 s
+  readHex' = fst . head . readHex
+  r        = readHex' . take 2 $ s'
+  g        = readHex' . take 2 . drop 2 $ s'
+  b        = readHex' . take 2 . drop 4 $ s'
+
+col2string :: Color -> String
+col2string (Triple (r, g, b)) = "#" ++ showHex' r ++ showHex' g ++ showHex' b
+ where
+  showHex' n =
+    let result = showHex n ""
+    in  if length result == 1 then "0" ++ result else result
+
+darken :: Color -> Color
+darken = shift (-50)
+
+brighten :: Color -> Color
+brighten = shift 50
+
+shift :: Int -> Color -> Color
+shift amt = fmap colshift
+ where
+  colshift x =
+    if x + amt > 255 then 255 else if x + amt < 0 then 0 else x + amt
+
+gradientStr :: Color -> Color -> String
+gradientStr c1 c2 =
+  " " ++ bg left ++ " " ++ bg mid ++ " " ++ bg right ++ " " ++ bg c2 ++ " "
+ where
+  mid   = avgColor c1 c2
+  left  = avgColor c1 mid
+  right = avgColor c2 mid
+
+avgColor :: Color -> Color -> Color
+avgColor c1 c2 = fmap (`div` 2) $ (+) <$> c1 <*> c2
+
+dkstGrey, dkerGrey, ltstGrey, lterGrey :: Color
+dkstGrey = string2col "#121212"
+
+dkerGrey = string2col "#303030"
+
+ltstGrey = string2col "#d0d0d0"
+
+lterGrey = string2col "#585858"
+
+textGrey = string2col "#9e9e9e"
+
+fdBlue, fdGrey, brBlue, brGrey :: Color
+fdBlue = string2col "#00627f"
+
+fdGrey = string2col "#8ea5ab"
+
+brBlue = string2col "#0097af"
+
+brGrey = string2col "#d2eaf0"
+
+orange :: Color
+orange = string2col "#7d5b39"
+
+fg, bg :: Color -> String
+bg x = "^bg(" ++ col2string x ++ ")"
+
+fg x = "^fg(" ++ col2string x ++ ")"
+
+fgbg :: Color -> Color -> String
+fgbg f b = fg f ++ bg b
--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,6 +1,18 @@
 -- Use /usr/share/xmonad-2.9/ghc-6.12.3/man/xmonad.hs as a reference
 -- A good resource for dzen config: http://thinkingeek.com/2011/11/21/simple-guide-configure-xmonad-dzen2-conky/
 -- http://lynnard.me/blog/2013/11/05/building-a-vim-like-xmonad-prompt-task-groups-topical-workspaces-float-styles-and-more/
+import           Bar.Colors                     ( brighten
+                                                , col2string
+                                                , dkerGrey
+                                                , dkstGrey
+                                                , fg
+                                                , fgbg
+                                                , gradientStr
+                                                , lterGrey
+                                                , ltstGrey
+                                                , orange
+                                                , textGrey
+                                                )
 import qualified Data.Map                      as M
 import           Data.Maybe
 import           Data.Ratio                     ( (%) )
@@ -81,8 +93,6 @@ import           XMonad.Hooks.FadeInactive
 
 import           Data.Monoid
 import qualified XMonad.Util.ExtensibleState   as XS
-import           Numeric                        ( readHex
-                                                , showHex
 import           Utils                          ( getOffset
                                                 , getResolution
                                                 , ioIconHeight
@@ -94,7 +104,6 @@ import           Startup.Utils
 
 desktopHost, laptopHost :: String
 desktopHost = "franky"
-
 laptopHost = "krang"
 
 maxTitleLen :: Int
@@ -208,88 +217,6 @@ toggleFloat = withFocused $ \w -> do
 
 ----------------------------------------------------------------------
 -- Status bars and logging
-data Triple a = Triple (a, a, a)
-
-instance Functor Triple where
-  fmap f (Triple (x, y, z)) = Triple (f x, f y, f z)
-
-instance Applicative Triple where
-  pure x = Triple (x, x, x)
-  (Triple (f1, f2, f3)) <*> (Triple (x1, x2, x3)) =
-    (Triple (f1 x1, f2 x2, f3 x3))
-
-type Color = Triple Int
-
-string2col :: String -> Color
-string2col s = Triple (r, g, b)
- where
-  s'       = drop 1 s
-  readHex' = fst . head . readHex
-  r        = readHex' . take 2 $ s'
-  g        = readHex' . take 2 . drop 2 $ s'
-  b        = readHex' . take 2 . drop 4 $ s'
-
-col2string :: Color -> String
-col2string (Triple (r, g, b)) = "#" ++ showHex' r ++ showHex' g ++ showHex' b
- where
-  showHex' n =
-    let result = showHex n ""
-    in  if length result == 1 then "0" ++ result else result
-
-darken :: Color -> Color
-darken = shift (-50)
-
-brighten :: Color -> Color
-brighten = shift 50
-
-shift :: Int -> Color -> Color
-shift amt = fmap colshift
- where
-  colshift x =
-    if x + amt > 255 then 255 else if x + amt < 0 then 0 else x + amt
-
-gradientStr :: Color -> Color -> String
-gradientStr c1 c2 =
-  " " ++ bg left ++ " " ++ bg mid ++ " " ++ bg right ++ " " ++ bg c2 ++ " "
- where
-  mid   = avgColor c1 c2
-  left  = avgColor c1 mid
-  right = avgColor c2 mid
-
-avgColor :: Color -> Color -> Color
-avgColor c1 c2 = fmap (`div` 2) $ (+) <$> c1 <*> c2
-
-dkstGrey, dkerGrey, ltstGrey, lterGrey :: Color
-dkstGrey = string2col "#121212"
-
-dkerGrey = string2col "#303030"
-
-ltstGrey = string2col "#d0d0d0"
-
-lterGrey = string2col "#585858"
-
-textGrey = string2col "#9e9e9e"
-
-fdBlue, fdGrey, brBlue, brGrey :: Color
-fdBlue = string2col "#00627f"
-
-fdGrey = string2col "#8ea5ab"
-
-brBlue = string2col "#0097af"
-
-brGrey = string2col "#d2eaf0"
-
-orange :: Color
-orange = string2col "#7d5b39"
-
-fg, bg :: Color -> String
-bg x = "^bg(" ++ col2string x ++ ")"
-
-fg x = "^fg(" ++ col2string x ++ ")"
-
-fgbg :: Color -> Color -> String
-fgbg f b = fg f ++ bg b
-
 rAlignCmpnt :: Int -> Logger -> Logger
 rAlignCmpnt = fixedWidthL AlignRight " "
 
