9357965111fc36143fe010f78acea35c44a5e2bc
qfjp <qfjp@users.noreply.github.com>
2025-03-19 18:22:09 -0400

    Move haphazard files in `bin' to proper project dirs.
    Flatten the folder structure, e.g.
    bin/macros/vol-control -> bin/vol-control
      - screen-lock/bin/autolock.sh
      - tmux/.tmux.conf
      - xmonad/.xmonad/xmonad.hs

--- screen-lock/bin/autolock.sh
+++ screen-lock/bin/autolock.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-exec xautolock -detectsleep -time 10 -locker $HOME/bin/screensaver/lock_screen \
+exec xautolock -detectsleep -time 10 -locker $HOME/bin/lock_screen \
                -notify 30 -notifier "notify-send -u normal -t 30000 -- 'LOCKING screen in 30 seconds'"
