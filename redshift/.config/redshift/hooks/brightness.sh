f3c71d32875f4264fec978ddfbb0955334cd30c5
qfjp <qfjp@users.noreply.github.com>
2025-03-22 19:30:37 -0400

    Update brightness hook to account for multiple devices.
    Probably hasn't been tested, and now with the move to wayland it
    probably never will be.

--- redshift/.config/redshift/hooks/brightness.sh
+++ redshift/.config/redshift/hooks/brightness.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Set brightness via xbrightness when redshift status changes
 
@@ -7,19 +7,30 @@
 brightness_day=80
 brightness_transition=50
 brightness_night=10
-# Set fade time for changes to one minute
-fade_time=60000
+# Set fps for smooth transition
+fps=1000
+#fade_time=60000
+# Adjust this grep to filter only the backlights you want to adjust
+mapfile -t backlights < <(xbacklight -list)
+echo "${backlights[@]}"
+
+set_brightness() {
+    for backlight in "${backlights[@]}"; do
+        xbacklight -set "$1" -fps $fps -ctrl "$backlight" &
+        echo xbacklight -set "$1" -fps $fps -ctrl "$backlight"
+    done
+}
 
 if [ "$1" = period-changed ]; then
     case $3 in
         night)
-            xbacklight -set $brightness_night -time $fade_time
+            set_brightness $brightness_night
             ;;
         transition)
-            xbacklight -set $brightness_transition -time $fade_time
+            set_brightness $brightness_transition
             ;;
         daytime)
-            xbacklight -set $brightness_day -time $fade_time
+            set_brightness $brightness_day
             ;;
     esac
 fi
